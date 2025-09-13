24a1c993a3a1aef398a1f2cd8fca493bf9c69d90
MMartinJ <marina.m.jurado86@gmail.com>
2025-09-13 11:25:42 +0200

    13 de septiembre

--- 135/modificar.php
+++ 135/modificar.php
@@ -1,22 +1,40 @@
 <?php
-// Modificar registro de una BD
-$num = $_POST["num"];
-$nombre = $_POST["nombre"];
-$telefono = $_POST["telefono"];
-echo $telefono;
-if (isset($num)){
+
 // conecta con la BD
-// Conectamos a la BD
-$link = mysql_connect("localhost","nhjukvlb_ejemplo","phpya101","");
-mysql_select_db("nhjukvlb_libro101", $link);
-
-$sql = "UPDATE agenda SET nombre='$nombre', telefono='$telefono' WHERE id=$num";
-$result = mysql_query($sql);
-}else{
-echo "Debe especificar un 'Numero'.\n";
+try{
+    $pdo = new pdo(
+        'mysql:host=localhost;dbname=test;charset=utf8mb4', // DSN de conexión (Data Source Name)
+        'root',                                             // Usuario de MySQL
+        ''                                                  // Contraseña de MySQL (vacía en este ejemplo)
+    );
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
-echo "Los datos se han Modificado con exito";
+
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telf'];
+$num = $_POST['num'];
+
+$query = "UPDATE agenda SET nombre=:nombre, telefono=:telefono WHERE id_agenda=:id_agenda";
+
+$stmt = $pdo->prepare($query);
+
+// Ejecutamos pasando los valores en un array asociativo
+try{
+    $stmt->execute([
+        ':nombre'  => $nombre,
+        ':telefono'=> $telefono,
+        ':id_agenda'=> $num
+]);
+} catch (PDOException $e) {
+    die("Error en la inserción: " . $e->getMessage());
+}
+
+echo "Registro modificado correctamente";
+
+// Cerramos la conexión (opcional en PHP, se cierra al finalizar el script)
+$pdo = null;
+
+
+
 ?>
-
-
-
--- 136/borrar.php
+++ 136/borrar.php
@@ -1,24 +1,19 @@
 <?php
-// Eliminar un registro - recibo el dato del registro de la BD a eliminar
-$borrar = $_GET['borrar'];
-$mysqli = new mysqli("localhost", "nhjukvlb_ejemplo", "phpya101", "nhjukvlb_libro101");
 
-// Verificar la conexión
-if ($mysqli->connect_errno) {
-    echo "Error al conectar con la base de datos: " . $mysqli->connect_error;
-    exit;
-}
 
-if (isset($borrar)) {
-    $sql = "DELETE FROM agenda WHERE id = $borrar";
-    if ($mysqli->query($sql)) {
-        echo "Los datos se han borrado con éxito";
-    } else {
-        echo "Error al borrar los datos: " . $mysqli->error;
-    }
-} else {
-    echo "Debe especificar un 'id'.";
-}
-// Cerrar la conexión
-$mysqli->close();
+    $dsn = "mysql:host=localhost;dbname=test";
+    $pdo = new PDO($dsn,'root','');
+
+    $sql = "DELETE FROM agenda WHERE id_agenda=:id";
+    $stmt = $pdo->prepare($sql);
+
+    $id_agenda = $_POST['id'];
+
+    $stmt->execute(
+        ['id' => $id_agenda]
+    );
+
+    echo "El registro se ha borrado correctamente";
+
+
 ?>
